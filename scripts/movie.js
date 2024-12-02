d5c0b5226d2c289552eb1cd1ac715955c624a629
FES001 <firdavs7794948@gmail.com>
2024-12-03 01:22:06 +0500

    Working on it.

--- scripts/movie.js
+++ scripts/movie.js
@@ -1,15 +1,22 @@
 const movieInput = document.getElementById('movieInput');
 const outputContainer = document.querySelector('.main-content-itself-inner-movie-output');
+const outputItself = document.querySelectorAll('.main-content-itself-inner-movie-output-itself');
+const outputItselfImage = document.querySelectorAll('.main-content-itself-inner-movie-output-itself-image-itself');
+const outputItselfName = document.querySelectorAll('.main-content-itself-inner-movie-output-itself-info-name');
+const outputItselfRating = document.querySelectorAll('.main-content-itself-inner-movie-output-itself-info-rating');
+
+const movieSelected = ["tt11126994","tt0765010","tt0107048","tt1677720","tt1068680","tt0816692","tt0162222","tt1790864"]
 
 // RETRIEVING THE DATA FROM AN API
 
 async function displayTheData() {
-    const response = await fetch(`https://www.omdbapi.com/?t=${movieInput.value}&apikey=d5f9f26`);
-    const movieData = await response.json();
-
     // CREATE AN ELEMENT
-    if (movieData) {
-        outputContainer.innerHTML = `
+    for (let i = 0; i < movieSelected.length; i++) {
+        // FETCHING DATA
+        const response = await fetch(`http://www.omdbapi.com/?i=${movieSelected[i]}&apikey=1512de31`);
+        const movieData = await response.json();
+        // USING THE FETCHED DATA
+        outputContainer.innerHTML += `
             <a href="" class="main-content-itself-inner-movie-output-itself">
                 <div class="main-content-itself-inner-movie-output-itself-image">
                     <img src="${movieData.Poster}" class="main-content-itself-inner-movie-output-itself-image-itself">
@@ -20,12 +27,15 @@ async function displayTheData() {
                 </div>
             </a>
         `;
-    } else {
-        outputContainer.innerHTML = '';
     };
-
 };
 
-// INITIALIZING INPUT 
 
-movieInput.addEventListener('input', displayTheData);
+async function  searchMovie() {
+    const response = await fetch(`https://www.omdbapi.com/?t=${movieInput.value}&apikey=1512de31`);
+    const movieData = await response.json();
+}
+
+// INITIALIZING BUTTONS
+window.addEventListener('DOMContentLoaded', displayTheData);
+movieInput.addEventListener('input', searchMovie);
